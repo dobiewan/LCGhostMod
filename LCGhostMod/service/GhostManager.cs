f0587ff5c87c4ce4d47cd7ca41178c1040fbf1cc
Sarah <sarahdobienz@gmail.com>
2025-01-03 13:26:24 +1300

    More test events

--- LCGhostMod/service/GhostManager.cs
+++ LCGhostMod/service/GhostManager.cs
@@ -20,7 +20,8 @@ namespace Dobes
 			m_ghostSfxPlayer = new PlayerGhostSfxPlayer();
 
 			LethalNetworkMessages.s_ghostEventMessage.OnReceived += ReceiveGhostEvent;
-			LethalNetworkMessages.s_testEventMessage.OnReceived += ReceiveTestEvent;
+			LethalNetworkMessages.s_testEventMessageServer.OnReceived += ReceiveTestEventServer;
+			LethalNetworkMessages.s_testEventMessageClient.OnReceived += ReceiveTestEventClient;
 			
             Plugin.Log.LogInfo("GhostManager initialized!");
         }
@@ -28,15 +29,22 @@ namespace Dobes
 		private void OnDestroy()
 		{
 			LethalNetworkMessages.s_ghostEventMessage.OnReceived -= ReceiveGhostEvent;
-			LethalNetworkMessages.s_testEventMessage.OnReceived -= ReceiveTestEvent;
+			LethalNetworkMessages.s_testEventMessageServer.OnReceived -= ReceiveTestEventServer;
+			LethalNetworkMessages.s_testEventMessageClient.OnReceived -= ReceiveTestEventClient;
 		}
+		
+		private float m_timeOfLastTestEvent = 0f;
 
 		private void Update()
 		{
-			if (StartOfRound.Instance.localPlayerController.isCrouching)
+			if (StartOfRound.Instance.localPlayerController.isCrouching && Time.time - m_timeOfLastTestEvent >= 5f)
 			{
 				Plugin.Log.LogInfo("Sending test event");
-				LethalNetworkMessages.s_testEventMessage.SendAllClients("hello!");
+				LethalNetworkMessages.s_testEventMessageClient.SendServer("hello client!");
+				LethalNetworkMessages.s_testEventMessageServer.SendServer("hello server!");
+				LethalNetworkMessages.customClientMessage.SendServer("hello server!");
+				LethalNetworkMessages.customClientMessage.SendAllClients("hello all clients!");
+				LethalNetworkMessages.customServerMessage.SendAllClients("hello all clients!");
 			}
 		}
 
@@ -45,9 +53,24 @@ namespace Dobes
 			m_ghostEventDetector.Simulate();
 		}
 
-		private void ReceiveTestEvent(string obj)
+		private void ReceiveTestEventServer(string obj)
 		{
-			Plugin.Log.LogInfo("Received test event: " + obj);
+			Plugin.Log.LogInfo("Received server test event: " + obj);
+		}
+
+		private void ReceiveTestEventClient(string obj)
+		{
+			Plugin.Log.LogInfo("Received client test event: " + obj);
+		}
+
+		public static void ReceiveFromServer(string obj)
+		{
+			Plugin.Log.LogInfo("Received server test event 2: " + obj);
+		}
+
+		public static void ReceiveFromClient(string obj, ulong id)
+		{
+			Plugin.Log.LogInfo("Received client test event 2: " + obj);
 		}
 
 		private void TriggerGhostEvent(PlayerControllerB forPlayer)
--- LCGhostMod/service/LethalNetworkMessages.cs
+++ LCGhostMod/service/LethalNetworkMessages.cs
@@ -6,5 +6,9 @@ namespace Dobes;
 internal static class LethalNetworkMessages
 {
     internal static readonly LethalClientMessage<GhostEventData> s_ghostEventMessage = new LethalClientMessage<GhostEventData>(identifier: "GhostEvent");
-    internal static readonly LethalClientMessage<string> s_testEventMessage = new LethalClientMessage<string>(identifier: "TestEvent");
+    internal static readonly LethalClientMessage<string> s_testEventMessageClient = new LethalClientMessage<string>(identifier: "TestEvent");
+    internal static readonly LethalClientMessage<string> s_testEventMessageServer = new LethalClientMessage<string>(identifier: "TestEvent");
+    
+    public static LethalClientMessage<string> customClientMessage = new("TestEvent2", onReceived: GhostManager.ReceiveFromServer, onReceivedFromClient: GhostManager.ReceiveFromClient);
+    public static LethalServerMessage<string> customServerMessage = new("TestEvent2", onReceived: GhostManager.ReceiveFromClient);
 }
